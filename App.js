c536aadbc54769147e8ecd7c6366823ec1ebca9a
Lukasz Adamczyk <lukasz012798@gmail.com>
2020-11-02 21:01:19 +0100

    Permissions service - November 2

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+import * as ImagePicker from "expo-image-picker";
 
 import Button from "./components/Button";
 
 export default function App() {
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) alert("Permissions not granted!");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Button title={"Wybierz zdjęcie"} />
